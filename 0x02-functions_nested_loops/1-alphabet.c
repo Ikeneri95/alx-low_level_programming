65bea5a8cd70b8cab8bdc1e1cc49263c38bab978
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-13 12:49:19 +0100

    Alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet:This program prints alphabet in lowercare 
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putcher(j);
+	}
+	_putchar('\n');
+}
