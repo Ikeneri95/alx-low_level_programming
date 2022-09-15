82034735250d4d1aeeb83bf475c546dcde9f69a5
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-15 13:42:08 +0100

    Print numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - Print the numbers from 0 to 9
+*
+*/
 
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+
+}
