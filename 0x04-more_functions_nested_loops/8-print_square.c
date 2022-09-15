30256693c2599c6d15d00561a5c3a4a7c040a2b6
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-15 14:09:38 +0100

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,23 @@
+#include "main.h"
+/**
+ *print_square - print square followed by a new line
+ *
+ *@size: the size of the square
+ */
+void print_square(int size)
+{
+	int i, j ;
+	
+	if (size != 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
 
