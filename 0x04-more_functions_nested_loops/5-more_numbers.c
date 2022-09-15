f52816bae9247c9c9b5f427e13e8bdecd467336f
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-15 13:47:40 +0100

    Print 10 times the number from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Print 10 times the numbers, from 0 to 140
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
