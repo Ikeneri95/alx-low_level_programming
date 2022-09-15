7503c273b93f4b983707277f07be476152be8bb1
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-15 17:15:05 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - function that reverses a string.
+ * @s: String
+ */
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+
+	}
+}
