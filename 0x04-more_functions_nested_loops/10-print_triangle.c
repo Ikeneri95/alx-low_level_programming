04cfdab467e0b16fdaf3d71550dfd1d027afd87b
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-15 15:46:33 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+*print_triangle - Print a triangle
+*@size: size of the triangle
+*/
+void print_triangle(int size)
+{
+	int row, column, k;
 
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column--)
+			_putchar(' ');
+		for (k = row + column; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
