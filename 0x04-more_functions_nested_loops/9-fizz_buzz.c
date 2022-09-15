892af2e2f8b07f7921e68a124aed4f0e0fb445be
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-16 00:13:33 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - fizz buzz test
- *
+ * main - program that prints numbers from 1 to 100,
+ * followed by a new line
+ * 3 multiples to print Fizz
+ * 5 multiples to print Buzz
+ * 3 and 5 joins FizzBuzz
  * Return: 0
  */
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz\t");
-		else if ((i % 3) == 0)
-			printf("Fizz\t");
-		else if ((i % 5) == 0)
-			printf("Buzz'\t");
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
 		else
-			printf("%d\t", i);
+			printf("%d ", i);
 	}
+	printf("\n");
 	return (0);
 }
