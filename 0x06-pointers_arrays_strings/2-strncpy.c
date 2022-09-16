5cfcc6ae10cc0f2aa9797842870c73b81e45a0d0
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-16 18:09:44 +0100

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncpy - A [rogram that copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: This is the maximum number of characters to be appended
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+	
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	for (; b< n; b++)
+	{
+		dest[b] = '\0';
+		
+	}
+	return (dest);
+}
