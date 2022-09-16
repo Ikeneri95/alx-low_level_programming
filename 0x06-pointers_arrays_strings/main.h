f1ab1038f3fcfc64432ce49acc7e0bca37aeb442
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-16 18:33:53 +0100

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *leet(char *);
+int _putchar(char c);
+void print_number(int n);
+char *cap_string(char str[], int n);
+char *rot13(char *s);
