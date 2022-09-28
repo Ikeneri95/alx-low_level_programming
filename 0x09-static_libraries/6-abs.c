6da5d69f5c9c2ac2fc57da90acaa8987af2876c6
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-28 21:15:48 +0100

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Return the absolute value
+ * @n: number to check the absolute value
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
