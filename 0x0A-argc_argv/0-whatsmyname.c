71a297f5efce46cea0878f0b95f4b8a2a78cd900
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-09-30 10:46:15 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name followed by a new line
+ * @argc: Argument count
+ * @argv: Array of strings, Argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
