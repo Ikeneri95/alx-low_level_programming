6da48a5bd62ef4241fbf8195903f217377d739a4
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-10-06 23:29:23 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,42 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- *
- * @ptr: is a pointer to the memory previously allocated
- * @old_size: is the size, in bytes, of the allocated space for ptr
- * @new_size: is the new size, in bytes of the new memory block
- *
- * Return: pointer to new memory location
+ * *_realloc - Entry point
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated ptr
+ * @new_size: size of new memory block
+ * Return: a pointer to an array
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *alloc, *ptr2;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		alloc = malloc(new_size);
-		if (alloc == NULL)
-			return (NULL);
-		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	alloc = malloc(new_size);
-	ptr2 = ptr;
-	if (old_size > new_size)
-		old_size = new_size;
-	for (i = 0; i < old_size; i++)
-		alloc[i] = ptr2[i];
-	free(ptr);
-	return (alloc);
+char *array = NULL;
+char *p = NULL;
+unsigned int i;
+char *px = NULL;
+if (ptr == NULL)
+{
+array = malloc(new_size);
+if (array == NULL)
+return (NULL);
+return (array);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (new_size ==  old_size)
+return (ptr);
+p = malloc(new_size);
+if (p != NULL)
+{
+px = (char *)ptr;
+for (i = 0; i < old_size; i++)
+p[i] = px[i];
+free(ptr);
+return (p);
+}
+return (NULL);
 }
