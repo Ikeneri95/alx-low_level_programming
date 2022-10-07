c4ce40caaebf7bfa47a58f887dbcc1a4bf495adc
Ikeneri95 <kelechikennedy8@gmail.com>
2022-10-06 18:57:43 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,6 +25,7 @@ int is_digit(char *s)
  * @s: string to evaluate
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -40,6 +41,7 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
 	printf("Error\n");
@@ -52,7 +54,9 @@ void errors(void)
  * @argv: array of arguments
  * Return: always 0 (Success)
  */
+
 int main(int argc, char *argv[])
+
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
@@ -87,11 +91,11 @@ int main(int argc, char *argv[])
 		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');
+			putchar(result[i] + '0');
 	}
 	if (!a)
-		_putchar('0');
-	_putchar('\n');
+		putchar('0');
+	putchar('\n');
 	free(result);
 	return (0);
 }
