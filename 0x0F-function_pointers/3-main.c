c8c7f60a467f7365a144f104b4ab00d1c177f290
Kennedy Ikeneri <111211178+Ikeneri95@users.noreply.github.com>
2022-10-08 14:37:26 +0100

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Prints simple operations.
+ * @argc: number of arguments
+ * @argv: pointers to the arguments.
+ * Return: calculated function
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (!(get_op_func(argv[2])))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(a, b));
+	return (0);
+}
